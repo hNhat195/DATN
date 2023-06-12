07b1acac3cb3c88e6ad3962001e9156a0bcfec73
phuvd1609 <phuvd16092001@gmail.com>
2023-06-12 19:17:05 +0700

    edit home page

--- dclv-main/src/page/HomePage/components/Product.js
+++ dclv-main/src/page/HomePage/components/Product.js
@@ -4,14 +4,21 @@ import { Grid } from '@material-ui/core'
 //import path from 'src/constants/path'
 //import { Product as ProductType } from 'src/types/product.type'
 //import { formatCurrency, formatNumberToSocialStyle, generateNameId } from 'src/utils/utils'
+import { useHistory } from "react-router-dom";
+
 
 export default function Product({ product }) {
+  const history = useHistory();
+  const handleClick = (orderId) => {
+    console.log(orderId)
+    history.push(`/order/orderDetail/${orderId}`);
+  }
   return (
-    <Grid>
+    <Grid onClick={() => handleClick(product._id)}>
       <div className='overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.04rem] hover:shadow-md'>
         <div className='relative w-full pt-[100%]'>
           <img
-            src={product.image}
+            src={"./cat.jpg"}
             alt
             className='absolute top-0 left-0 h-full w-full bg-white object-cover'
           />
