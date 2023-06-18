2bb9f42f85dd6a961009876e487503460326f809
Trần Quang Khả <tranquangkha@MacBook-Pro-cua-EQ-0624.local>
2023-06-18 11:21:33 +0700

    add product detail page and components for selling pages

--- dclv-main/src/page/ProductDetail/index.js
+++ dclv-main/src/page/ProductDetail/index.js
@@ -1,8 +1,124 @@
 import React, { useState } from "react";
+import { Add, Remove } from "@material-ui/icons";
+import styled from "styled-components";
+import Announcement from "../../components/SellingPages/Announcement";
+import Footer from "../../components/SellingPages/Footer";
+import Navbar from "../../components/SellingPages/Navbar";
+import Newsletter from "../../components/SellingPages/Newsletter";
+import { mobile } from "../../responsive";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const ProductDetailPage = () => {
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+  ${mobile({ padding: "10px", flexDirection: "column" })}
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+  ${mobile({ height: "40vh" })}
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+  ${mobile({ padding: "10px" })}
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ width: "100%" })}
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ width: "100%" })}
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
+
+const Product = () => {
   const [selectedImage, setSelectedImage] = useState(
     "https://cdn.shopify.com/s/files/1/0504/0986/5414/products/ZQ_Merino_Chestnut_Swatch_1000x1000.jpg?v=1680565550"
   );
@@ -12,25 +128,77 @@ const ProductDetailPage = () => {
     "https://cdn.shopify.com/s/files/1/0504/0986/5414/products/ZQ_Merino_Poppy_Roll_1000x1000.jpg?v=1686527461",
   ];
 
-  const handleThumbnailClick = (image) => {
-    setSelectedImage(image);
+  const fabric = {
+    name: "Organic Linen - Lilac",
+    fabricType: "Organic Linen",
+    color: "Lilac",
+    price: "100",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
+            iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
+            tristique tortor pretium ut. Curabitur elit justo, consequat id
+            condimentum ac, volutpat ornare.`,
   };
 
   return (
-    <div className="thumbnail-images" style={{ height: 700, width: 700 }}>
-      <Carousel
-        showStatus={false}
-        selectedItem={additionalImages.indexOf(selectedImage)}
-        onChange={(index) => handleThumbnailClick(additionalImages[index])}
-      >
-        {additionalImages.map((image, index) => (
-          <div key={index}>
-            <img src={image} alt={`Thumbnail Image ${index + 1}`} />
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <div className="thumbnail-images">
+            <Carousel
+              showStatus={false}
+              selectedItem={additionalImages.indexOf(selectedImage)}
+              onChange={
+                (index) => console.log("kkkkkkkkk")
+                // handleThumbnailClick(additionalImages[index])
+              }
+            >
+              {additionalImages.map((image, index) => (
+                <div key={index}>
+                  <img src={image} alt={`Thumbnail ${index + 1}`} />
+                </div>
+              ))}
+            </Carousel>
           </div>
-        ))}
-      </Carousel>
-    </div>
+        </ImgContainer>
+        <InfoContainer>
+          <Title>{fabric.name}</Title>
+          <Desc>{fabric.description}</Desc>
+          <Price>$ {fabric.price}</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
   );
 };
 
-export default ProductDetailPage;
+export default Product;
