0d612a24cdac9054f7d91bd9552bd6951920e0e4
Trần Quang Khả <tranquangkha478@gmail.com>
2023-06-24 20:37:46 +0700

    add fabricType, color, descriptions to FabricRoll schema

--- dclv-server-master/src/models/FabricRoll.js
+++ dclv-server-master/src/models/FabricRoll.js
@@ -11,9 +11,13 @@ const FabricRollSchema = new Schema(
       ref: "FabricType",
       require: true,
     },
+    fabricType: { type: String, require: true },
     colorId: { type: mongoose.Types.ObjectId, ref: "Color", require: true },
+    color: { type: String, require: true },
+
     price: { type: Number, require: true },
     image: [{ type: String, default: "" }],
+    descriptions: { type: String, default: "" },
   },
   { collection: "FabricRoll" }
 );
