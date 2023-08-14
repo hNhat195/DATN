4fd1a080a5d8cd96a56e3ebdc51392e0d1c57f34
hNhat195 <133624997+hNhat195@users.noreply.github.com>
2023-08-14 16:18:25 +0700

    fix

--- dclv-server-master/index.js
+++ dclv-server-master/index.js
@@ -12,23 +12,22 @@ const port = process.env.BACK_END_PORT;
 const mongodb_url = process.env.MONGODB_URL_V2;
 
 const router = require("./src/routes/routes");
-const { sendMailSuccess } = require("./src/controller/mailer");
-const { exportBill } = require("./src/controller/exportBill");
 
 const app = express();
+app.set(port || 80);
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
 
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 mongoose
   .connect(mongodb_url, { useNewUrlParser: true })
   .then(() => {
     console.log("Connect to MongoDB successfull!");
-    app.listen(port, () => {
-      console.log(`Server is running at http://localhost:${port}`);
-    });
   })
   .catch((error) => {
     console.log("Connect to MongoDB failed!" + error);
