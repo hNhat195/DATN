400064e2b5e1279aaa5c9507b3205a5db15b7c2a
hNhat195 <133624997+hNhat195@users.noreply.github.com>
2023-08-16 12:07:32 +0700

    add code

--- dclv-server-master/index.js
+++ dclv-server-master/index.js
@@ -19,16 +19,17 @@ var rollbar = new Rollbar({
 });
 
 const app = express();
-app.set(port || 80);
+app.set("port", port || 80);
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
+app.listen(app.get("port"), () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
 mongoose
   .connect(mongodb_url, { useNewUrlParser: true })
   .then(() => {
